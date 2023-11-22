fac6f5cb8d1602742d21b8cea5a6c889358496e8
rarrieta <rarrieta@casewaresa.com>
2023-11-22 11:41:08 -0500

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+  import Pelicula from "./lib/Pelicula.svelte";
+  let peliculas = [];
+
+  const cargarPelicula = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/peliculas");
+      if (response.status == 200) {
+        const data = await response.json();
+        peliculas = data;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  cargarPelicula();
+</script>
+
+<div class="container">
+  <div class="grid">
+    {#each peliculas as pelicula}
+      <Pelicula {pelicula} />
+    {/each}
+  </div>
+</div>
+
+<style>
+</style>
---
+++ src/lib/Pelicula.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let pelicula;
+</script>
+
+<div class="movie-card">
+  <img class="movie-image" src={pelicula.portada} alt={pelicula.nombre} />
+  <div class="movie-details">
+    <h1 class="movie-title">{pelicula.nombre}</h1>
+  </div>
+</div>
