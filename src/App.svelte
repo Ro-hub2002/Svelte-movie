2e2a6306394fe4b02f89e9dbf2334ac147f2fd98
rarrieta <rarrieta@casewaresa.com>
2023-11-22 22:16:50 -0500

    Subiendo los cambios

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,8 @@
 <script>
   import Pelicula from "./lib/Pelicula.svelte";
   let peliculas = [];
+  let busqueda = "";
+  let resultados = [];
 
   const cargarPelicula = async () => {
     try {
@@ -14,13 +16,44 @@
     }
   };
   cargarPelicula();
+
+  function buscarPelicula() {
+    resultados = peliculas.filter((pelicula) =>
+      pelicula.nombre.toLowerCase().includes(busqueda.toLowerCase())
+    );
+  }
+  $: buscarPelicula();
+
+  
 </script>
 
-<div class="container">
-  <div class="grid">
-    {#each peliculas as pelicula}
-      <Pelicula {pelicula} />
-    {/each}
+<input
+  class="buscador"
+  type="text"
+  bind:value={busqueda}
+  placeholder="Buscar por nombre"
+  on:input={buscarPelicula}
+/>
+
+<div class="movie-container">
+  {#each peliculas as pelicula (pelicula.nombre)}
+    <Pelicula {pelicula} />
+  {/each}
+</div>
+
+<div class="sidebar" class:open>
+  <h3 class="fs-4">Géneros</h3>
+  <div class="botones-container">
+    <button class="boton" type="button">Acción</button>
+    <button class="boton" type="button">Drama</button>
+    <button class="boton" type="button">Aventura</button>
+    <button class="boton" type="button">Suspenso</button>
+    <button class="boton" type="button">Guerra</button>
+    <button class="boton" type="button">Misterio</button>
+    <button class="boton" type="button">Guerra</button>
+    <button class="boton" type="button">Ciencia Ficción</button>
+    <button class="boton" type="button">Musical</button>
+    <button class="boton" type="button">Crimen</button>
   </div>
 </div>
 
--- src/lib/Pelicula.svelte
+++ src/lib/Pelicula.svelte
@@ -3,8 +3,24 @@
 </script>
 
 <div class="movie-card">
-  <img class="movie-image" src={pelicula.portada} alt={pelicula.nombre} />
+  <img
+    class="movie-image"
+    src={pelicula.portada}
+    alt={pelicula.nombre}
+    loading="lazy"
+  />
   <div class="movie-details">
     <h1 class="movie-title">{pelicula.nombre}</h1>
+    <div class="movie-info">
+      <p><strong>Director:</strong> {pelicula.director}</p>
+      <p>
+        <strong>Director:</strong>
+        {pelicula.genero.charAt(0).toUpperCase() + pelicula.genero.slice(1)}
+      </p>
+      <p>
+        <strong>Año:</strong>
+        {new Date(pelicula.ano).getFullYear()}
+      </p>
+    </div>
   </div>
 </div>
